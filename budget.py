ced52403bf03a67b68e94d5765cd1022ae8aec69
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-02 07:28:02 -0500

    Implemented Category class with constructor.

---
+++ budget.py
@@ -0,0 +1,6 @@
+class Category:
+    # Constructor: Initializes a new instance of the Category class.
+    # Sets the category name and initializes an empty ledger list.
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
